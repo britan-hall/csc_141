07ae489a110cd6ba53922b0c607623edf355de12
britan-hall <britan.hall@icloud.com>
2024-08-29 08:15:39 -0400

    Add files via upload

---
+++ 01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,6 @@
+'''
+If I had infinite Python skills, I would like to know how to hack for ethical reasons. It would be very interesting to know how to
+reverse hack scammers and anyone trying to cause harm to those who simply don't understand. I would also like to help make software that
+better protects people from malware and online attacks.
+
+'''
