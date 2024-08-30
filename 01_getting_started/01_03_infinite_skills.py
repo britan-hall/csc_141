12cdc41978abe46a8321093db5c1e6e1628863bf
britan-hall <britan.hall@icloud.com>
2024-08-30 09:42:32 -0400

    01_03_infinite_skills.py

--- 01_getting_started/01_03_infinite_skills.py
+++ 01_getting_started/01_03_infinite_skills.py
@@ -1,6 +1,15 @@
 '''
-If I had infinite Python skills, I would like to know how to hack for ethical reasons. It would be very interesting to know how to
-reverse hack scammers and anyone trying to cause harm to those who simply don't understand. I would also like to help make software that
-better protects people from malware and online attacks.
+If I had infinite Python skills, I would like to know how to hack for ethical reasons. It would be very interesting to know 
+how to reverse-hack scammers and anyone trying to cause harm to those who simply don't understand. I would also like to help
+make software that better protects people from malware and online attacks. I think it would be of great help to a lot of people who 
+spend time online to use free software that is easy to use and truly protective against malware.
+
+Another project I would like to work on is building my own game. I have found a lot of inspiration from indie developers. I enjoy 
+playing these games and can feel how much creativity and thought went into them. I enjoy being creative, and developing a game 
+from scratch seems to interest me. I can’t say what the game would be about, as I play and enjoy a variety of games.
+
+I've always loved doing puzzles and complex problems where you can use your creativity to solve them. For instance, I first 
+discovered this interest while working with a group of students on a K'Nex competition. We were tasked with building a machine to 
+pick up and move a block. I believe computer science is the perfect combination of my passions for computers and problem-solving.
 
 '''
